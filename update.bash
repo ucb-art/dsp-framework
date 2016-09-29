799867a02d5434fc9fa5a016ecd12689746217af
Angie <shunshou@gmail.com>
2016-09-29 15:10:41 -0700

    added update script that does submodule update + publish local

---
+++ update.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+DIR=`dirname $0`
+git submodule update --init --recursive
+cd $DIR/firrtl ; sbt publish-local ; cd ..
+cd $DIR/firrtl-interpreter ; sbt publish-local ; cd ..
+cd $DIR/chisel3 ; sbt publish-local ; cd ..
+cd $DIR/chisel-testers ; sbt publish-local ; cd ..
+cd $DIR/dsptools ; sbt publish-local ; sbt test ; cd ..
+
