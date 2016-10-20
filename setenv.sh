950aaec42b50362045acdc616d7a36a3b7355ef6
Paul Rigge <rigge@berkeley.edu>
2016-10-20 10:56:51 -0700

    Add script to add firrtl to path.

---
+++ setenv.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export PATH=$PWD/firrtl/utils/bin:$PATH
+
--- update.bash
+++ update.bash
@@ -1,7 +1,8 @@
 #!/bin/bash
 DIR=`dirname $0`
 git submodule update --init --recursive
-cd $DIR/firrtl ; sbt publish-local ; cd ..
+cd $DIR/firrtl ; sbt publish-local ; sbt assembly; cd ..
+. setenv.sh
 cd $DIR/firrtl-interpreter ; sbt publish-local ; cd ..
 cd $DIR/chisel3 ; sbt publish-local ; cd ..
 cd $DIR/chisel-testers ; sbt publish-local ; cd ..
