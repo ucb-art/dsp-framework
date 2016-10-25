3695b8cd06552e94354c3df4763881fde3a4d599
Paul Rigge <rigge@berkeley.edu>
2016-10-25 13:12:41 -0700

    Split up update.bash into multiple scripts.
    I found I often wanted to publish-local my local changes, but the update
    script would git submodule update and wipe out my local changes. This
    commit lets you run publish-local.bash if you are doing local
    development.

--- update.bash
+++ update.bash
@@ -1,10 +1,4 @@
 #!/bin/bash
-DIR=`dirname $0`
 git submodule update --init --recursive
-cd $DIR/firrtl ; sbt publish-local ; sbt assembly; cd ..
-. setenv.sh
-cd $DIR/firrtl-interpreter ; sbt publish-local ; cd ..
-cd $DIR/chisel3 ; sbt publish-local ; cd ..
-cd $DIR/chisel-testers ; sbt publish-local ; cd ..
-cd $DIR/dsptools ; sbt publish-local ; sbt test ; cd ..
-
+source ./setenv.sh
+source ./publish-local.sh
