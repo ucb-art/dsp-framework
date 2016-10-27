c0b0c3c91de115cd6f5cee0a2ad4ef95837969e9
Stevo Bailey <stevo.bailey@eecs.berkeley.edu>
2016-10-27 01:15:51 -0700

    [stevo]: merge update scripts again, cause they buggy

--- publish-local.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-DIR=`dirname $0`
-cd $DIR/firrtl ; sbt publish-local ; sbt assembly; cd ..
-cd $DIR/firrtl-interpreter ; sbt publish-local ; cd ..
-cd $DIR/chisel3 ; sbt publish-local ; cd ..
-cd $DIR/chisel-testers ; sbt publish-local ; cd ..
-cd $DIR/dsptools ; sbt publish-local ; sbt test ; cd ..
--- setenv.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export PATH=$PWD/firrtl/utils/bin:$PATH
-
