17f14f3c8afdfeed0838af29716e07911c2b9275
Paul Rigge <rigge@berkeley.edu>
2016-11-03 15:33:09 -0700

    Don't run tests.

--- publish-local.bash
+++ publish-local.bash
@@ -5,4 +5,4 @@ cd $DIR/firrtl ; sbt publish-local ; sbt assembly; cd ..
 cd $DIR/firrtl-interpreter ; sbt publish-local ; cd ..
 cd $DIR/chisel3 ; sbt publish-local ; cd ..
 cd $DIR/chisel-testers ; sbt publish-local ; cd ..
-cd $DIR/dsptools ; sbt publish-local ; sbt test ; cd ..
+cd $DIR/dsptools ; sbt publish-local ; cd ..
