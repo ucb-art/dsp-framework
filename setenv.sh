8e50a20dc83c36dabe03bfa82c528ebb0b51eec0
Paul Rigge <rigge@berkeley.edu>
2016-12-06 12:46:32 -0800

    setenv.sh not needed- Makefiles invoke firrtl directly

--- setenv.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-export PATH=$PWD/rocket-chip/firrtl/utils/bin:$PATH
--- update.bash
+++ update.bash
@@ -1,5 +1,3 @@
 #!/bin/bash
 git submodule update --init
 cd rocket-chip; git submodule update --init; cd ..
-
-source ./setenv.sh
