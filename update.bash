9812ae90572d77c7e98e5590154ad640d9415804
Stevo Bailey <stevo.bailey@eecs.berkeley.edu>
2017-02-15 14:38:38 -0800

    [stevo]: trying more hwacha-less travis

--- update.bash
+++ update.bash
@@ -1,4 +1,8 @@
 #!/bin/bash
 git submodule update --init
-cd rocket-chip; git submodule update --init; cd ..
+if [ "$1" == "no_hwacha" ]; then
+  cd rocket-chip; cat .gitmodules | grep path | awk '{print $3}' | grep -v hwacha | xargs git submodule update --init; cd ..
+else
+  cd rocket-chip; git submodule update --init; cd ..
+fi
 
