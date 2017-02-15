efd6ab23691250a52014aea43f42f5b44b878b66
Stevo Bailey <stevo.bailey@eecs.berkeley.edu>
2017-02-15 15:16:17 -0800

    [stevo]: bad hwacha, dont do that

--- update.bash
+++ update.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 git submodule update --init
 if [ "$1" == "no_hwacha" ]; then
-  cd rocket-chip; cat .gitmodules | grep path | awk '{print $3}' | grep -v hwacha | xargs git submodule update --init; cd ..
+  cd rocket-chip; cat .gitmodules | grep path | awk '{print $3}' | grep -v hwacha | xargs git submodule update --init; rm src/main/scala/rocketchip/PrivateConfigs.scala; cd ..
 else
   cd rocket-chip; git submodule update --init; cd ..
 fi
